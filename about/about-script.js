8ff1cf9bc888c71da4108262c1f82f34c4b326e0
Kanishka-shetty <kanishkashetty12@gmail.com>
2025-06-24 11:15:03 +0530

    Changed url for fetch of footers

--- about/about-script.js
+++ about/about-script.js
@@ -1,5 +1,5 @@
 
-fetch("http://localhost:1337/api/aboutpages")
+fetch("https://meaningful-horse-99e25d03c1.strapiapp.com/api/heroabouts/footers/aboutpages")
   .then(res => res.json())
   .then(data => {
     const content = data.data[0]; // ✅ Add this line
--- footer/footer-script.js
+++ footer/footer-script.js
@@ -1,4 +1,4 @@
-fetch("https://meaningful-horse-99e25d03c1.strapiapp.com/api/heroabouts/footers")
+fetch("https://meaningful-horse-99e25d03c1.strapiapp.com/api/footers")
   .then(res => res.json())
   .then(data => {
     const footerData = data.data[0];
